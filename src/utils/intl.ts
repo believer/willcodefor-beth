15395f29534c45cda0d92c7bc5790198f18f9c7a
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-07-31 21:53:48 +0200

    chore: add time style options for date formatter

--- src/utils/intl.ts
+++ src/utils/intl.ts
@@ -13,7 +13,7 @@ export const toYear = (date: string | Date) =>
 export const toYearShort = (date: string | Date) =>
   dateFormatter({ year: '2-digit' }).format(new Date(date))
 
-export const formatDateTime = (date: string) =>
-  dateFormatter({ dateStyle: 'short', timeStyle: 'short' }).format(
-    new Date(date)
-  )
+export const formatDateTime = (
+  date: string,
+  timeStyle: Intl.DateTimeFormatOptions['timeStyle'] = 'short'
+) => dateFormatter({ dateStyle: 'short', timeStyle }).format(new Date(date))
