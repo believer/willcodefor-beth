955307fb43e9173a3868fb1f9078679921319b68
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-08-01 08:51:18 +0200

    chore: add indexes for til id and slug

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,23 +1,34 @@
 import { InferModel, relations, sql } from 'drizzle-orm'
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { index, integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-export const posts = sqliteTable('posts', {
-  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-  title: text('title').notNull(),
-  body: text('body').notNull(),
-  excerpt: text('excerpt').notNull(),
-  slug: text('slug').notNull().unique(),
-  longSlug: text('longSlug').notNull().unique(),
-  series: text('series'),
-  published: integer('published', { mode: 'boolean' }).notNull().default(false),
-  tilId: integer('tilId', { mode: 'number' }),
-  createdAt: text('createdAt')
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-  updatedAt: text('updatedAt')
-    .notNull()
-    .default(sql`CURRENT_TIMESTAMP`),
-})
+export const posts = sqliteTable(
+  'posts',
+  {
+    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    title: text('title').notNull(),
+    body: text('body').notNull(),
+    excerpt: text('excerpt').notNull(),
+    slug: text('slug').notNull().unique(),
+    longSlug: text('longSlug').notNull().unique(),
+    series: text('series'),
+    published: integer('published', { mode: 'boolean' })
+      .notNull()
+      .default(false),
+    tilId: integer('tilId', { mode: 'number' }),
+    createdAt: text('createdAt')
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+    updatedAt: text('updatedAt')
+      .notNull()
+      .default(sql`CURRENT_TIMESTAMP`),
+  },
+  (table) => {
+    return {
+      tilIdIdx: index('til_id_idx').on(table.tilId),
+      sludIdx: index('slug_idx').on(table.slug),
+    }
+  }
+)
 
 export const postsRelations = relations(posts, ({ many }) => ({
   views: many(postViews),
